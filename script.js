44a6a6ac6cc1d0d6c4e04ff3b18a35c02f90fa42
Isa <Isaismail3000@gmail.com>
2022-08-30 13:17:32 +0300

    Create functions to add, subtract, multiply, and divide

---
+++ script.js
@@ -0,0 +1,35 @@
+const add = function(a, b) {
+    let sum = a + b;
+    return sum;
+  };
+  
+  const subtract = function(a, b) {
+      let result = a - b;
+    return result;
+  };
+  
+  const sum = function(array) {
+    let result = array.reduce((a,b) => a+b, 0);
+    return result
+  }
+  
+  const multiply = function(array) {
+    let result = array.reduce((a,b) => a*b, 1);
+    return result
+  };
+  
+  const power = function(a, b) {
+      let result = a ** b;
+    return result;
+  };
+  
+  const factorial = function(a) {
+    let array = [];
+      for (let i = 1; i <= a; i++) {
+      array.push(i)
+      console.log(array)
+    }
+    let result = array.reduce((a,b) => a*b, 1);
+    return result;
+  };
+  
