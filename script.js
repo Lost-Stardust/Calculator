876efe5de02a308d84a59484b67879f342620a9d
Isa <Isaismail3000@gmail.com>
2022-08-30 22:41:39 +0300

    Add addition, subtraction, multiplication, division functions

--- script.js
+++ script.js
@@ -1,35 +1,23 @@
-const add = function(a, b) {
-    let sum = a + b;
-    return sum;
-  };
+function add(a, b) {
+  let result = a + b;
+  console.log(result);
+  return result;
+};
   
-  const subtract = function(a, b) {
-      let result = a - b;
-    return result;
-  };
+function subtract(a, b) {
+  let result = a - b;
+  console.log(result);
+  return result;
+};
   
-  const sum = function(array) {
-    let result = array.reduce((a,b) => a+b, 0);
-    return result
-  }
+function multiply(array) {
+  let result = array.reduce((a,b) => a*b, 1);
+  console.log(result);
+  return result
+}
   
-  const multiply = function(array) {
-    let result = array.reduce((a,b) => a*b, 1);
-    return result
-  };
-  
-  const power = function(a, b) {
-      let result = a ** b;
-    return result;
-  };
-  
-  const factorial = function(a) {
-    let array = [];
-      for (let i = 1; i <= a; i++) {
-      array.push(i)
-      console.log(array)
-    }
-    let result = array.reduce((a,b) => a*b, 1);
-    return result;
-  };
-  
+function divide(a, b) {
+  let result = a / b;
+  console.log(result);
+  return result
+}
