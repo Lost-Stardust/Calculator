694e2e70bde41f67b18b492de46673fcf7060f03
Isa <Isaismail3000@gmail.com>
2022-09-20 18:30:21 +0300

    Create global memArray, push displayValue to memArray, and make function
    the global memArray is used to get the value of displayValue out of
    function scope and usable globally. The new concat function is used to
    pass in the value of memArray that is parseInted to a local variable.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let memArray = [];
+
 function addition(a, b) {
   let result = a + b;
   console.log(result);
@@ -39,12 +41,22 @@ function operate(operator, num1, num2) {
 
 let display = document.querySelector('#display');
 let buttons = document.querySelectorAll('button')
-
+console.log(buttons);
 let array = [];
 let displayValue;
 function popDisplay(input) {
   displayValue = display.textContent = `${input}`;
   array.push(displayValue);
+  memArray.push(displayValue);
   console.log(array);
   display.innerHTML = array.join('');
+  concat(displayValue)
 };
+
+console.log(memArray);
+function concat(num) {
+  num = parseInt(memArray.join(''));
+  console.log(num);
+  console.log(typeof(num))
+
+}
