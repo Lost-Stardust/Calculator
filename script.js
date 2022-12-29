6fdad7df21bd313c9adba0eceb1dd5654ddd52fa
Isa <Isaismail3000@gmail.com>
2022-12-29 13:18:49 +0300

    Use correct comment blocks, add new comment, remove one console log

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/* create arithmetic functions */
+// create arithmetic functions 
+
 function add(x,y) {
   let addResult = x + y
   return addResult
@@ -16,22 +17,27 @@ function divide(x,y) {
   return divideResult
 }
 
-/*select all buttons and the display*/ 
+// select all buttons and the display
+
 let buttons = document.querySelectorAll("button")
 console.log(buttons)
 const display = document.querySelector("#display")
 console.log(display)
 
-/*add event listener to all the number buttons and 
-store the number clicked in a array and display it*/
+// add event listener to all the number buttons and display them
+
 const numArr = []
 for(let i=0;i<=9;i++) {
   buttons.item(i).addEventListener('click', () => {
     numArr.push(buttons.item(i).textContent)
-    console.log(numArr.join(''))
     display.textContent = (numArr.join(''))
   })
 }
+
+// for loop that loops through the operator buttons
+// stores the value on display on operator click
+// uses global variable to get the value out of scope
+
 let number1 = 0
 for(let i=9;i<=13;i++) {
   buttons.item(i).addEventListener('click', () => {
