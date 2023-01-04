ba1f3695e37bf30e066c372ba09b8197363a6858
Isa <Isaismail3000@gmail.com>
2023-01-04 18:19:07 +0300

    Fix num2 for loop not executing, modify num1 for loop for future Fix
    num1 for loop must only execute when operators are false, which is why
    the slight modification was made. Moved the operator variables above
    num1 for loop because of initialization error.

--- script.js
+++ script.js
@@ -24,35 +24,49 @@ console.log(buttons)
 const display = document.querySelector("#display")
 console.log(display)
 
-// add event listener to all the number buttons and display them
-
-const numArr = []
-let number1 = 0
-for(let i=0;i<=9;i++) {
-  buttons.item(i).addEventListener('click', () => {
-    numArr.push(buttons.item(i).textContent)
-    display.textContent = (numArr.join(''))
-    const num1 = display.textContent
-    number1 = num1
-    console.log(`num1 is ${number1}`)
-    console.log(add)
-  })
-}
-
-// for loop that loops through the operator buttons
-// stores the value on display on operator click
-// uses global variable to get the value out of scope
-
 let add = false
 let subtract = false
 let multiply = false
 let divide = false
 
+// add event listener to all the number buttons and display them
+
+const numArr = []
+let number1 = 0
+
+if((add == false ) && (multiply == false) 
+&& (divide == false) && (subtract == false)) {
+  for(let i=0;i<=9;i++) {
+    buttons.item(i).addEventListener('click', () => {
+      numArr.push(buttons.item(i).textContent)
+      display.textContent = (numArr.join(''))
+      const num1 = display.textContent
+      number1 = num1
+      console.log(`num1 is ${number1}`)
+      console.log(add)
+    })
+  }
+}
+// for(let i=0;i<=9;i++) {
+//   buttons.item(i).addEventListener('click', () => {
+//     numArr.push(buttons.item(i).textContent)
+//     display.textContent = (numArr.join(''))
+//     const num1 = display.textContent
+//     number1 = num1
+//     console.log(`num1 is ${number1}`)
+//     console.log(add)
+//   })
+// }
+
+// for loop that loops through the operator buttons
+// stores the value on display on operator click
+// uses global variable to get the value out of scope
+
+
 for(let i=10;i<=13;i++) {
   buttons.item(i).addEventListener('click', () => {
     buttons.item(i).style.backgroundColor = '#FFC480';
-    console.log(buttons.item(i))
-     
+    console.log(buttons.item(i).className)
   })
 }
 
@@ -61,39 +75,46 @@ buttons.item(10).addEventListener('click', () => {
   if(display.textContent != '0') {
     add = true;
     console.log( `add is ${add}`)
+    getNum2()
   }
 })
 buttons.item(11).addEventListener('click', () => {
   if(display.textContent != '0') {
     subtract = true;
     console.log( `subtract is ${subtract}`)
+    getNum2()
   }
 })
 buttons.item(12).addEventListener('click', () => {
   if(display.textContent != '0') {
     multiply = true;
     console.log( `multiply is ${multiply}`)
+    getNum2()
   }
 })
 buttons.item(13).addEventListener('click', () => {
   if(display.textContent != '0') {
     divide = true;
     console.log( `divide is ${divide}`)
+    getNum2()
   }
 })
 
 const numArr2 = []
 let number2 = 0;
-if((add == true ) || (multiply == true) 
-|| (divide == true) || (subtract == true)) {
-  for(let i=0;i<=9;i++) {
-    buttons.item(i).addEventListener('click', () => {
-      display.textContent = '';
-      numArr2.push(buttons.item(i).textContent)
-      display.textContent = (numArr2.join(''))
-      const num2 = display.textContent
-      number2 = num2
-      console.log(`num2 is ${number2}`)      
-    })
+function getNum2() {
+  if((add == true ) || (multiply == true) 
+  || (divide == true) || (subtract == true)) {
+    console.log('it works, kinda')
+    for(let i=0;i<=9;i++) {
+      buttons.item(i).addEventListener('click', () => {
+        display.textContent = '';
+        numArr2.push(buttons.item(i).textContent)
+        display.textContent = (numArr2.join(''))
+        const num2 = display.textContent
+        number2 = num2
+        console.log(`num2 is ${number2}`)      
+      })
+    }
   }
-} 
+}
